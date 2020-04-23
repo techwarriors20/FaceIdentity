cb706f23bfa94afe29648f956cb2c8b3b90d7f32
techwarriors20 <techwarriors20@gmail.com>
2020-04-24 01:57:19 +0530

    Azure server url  changed

--- src/app/uploadblob/uploadblob.component.ts
+++ src/app/uploadblob/uploadblob.component.ts
@@ -30,7 +30,7 @@ export class UploadblobComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
 
-    this.http.post('https://localhost:5001/api/Upload/UploadAzure', formData, {reportProgress: true, observe: 'events'})
+    this.http.post('https://faceupload.azurewebsites.net/api/Upload/UploadAzure', formData, {reportProgress: true, observe: 'events'})
       .subscribe(event => {
         if (event.type === HttpEventType.UploadProgress)
           this.progress = Math.round(100 * event.loaded / event.total);
